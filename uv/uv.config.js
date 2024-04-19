19ad2ee8aa7b9d33848b7af48f9bd623df59006f
Max Weichers <166840219+MaxWeichers@users.noreply.github.com>
2024-04-19 11:07:02 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/a/',
+    prefix: 'https://history.about.dansted.org/a/',
     bare: 'https://history.about.dansted.org/o/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
+    bundle: 'https://history.about.dansted.org/m/bundle.js?v=5',
     config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    sw: 'https://history.about.dansted.org/m/sw.js?v=4',
 };
